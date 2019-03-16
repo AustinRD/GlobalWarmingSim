da77e2e87d3e02e0761b2bc5d845159e01825924
AustinRD <austinrd95@gmail.com>
2019-03-16 18:21:03 -0400

    Project start.

--- app/src/main/java/com/example/austin/globalwarmingsim/InfoActivity.java
+++ app/src/main/java/com/example/austin/globalwarmingsim/InfoActivity.java
@@ -3,7 +3,7 @@ package com.example.austin.globalwarmingsim;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
-public class HowToActivity extends AppCompatActivity
+public class InfoActivity extends AppCompatActivity
 {
     @Override
     public void onCreate(Bundle savedInstanceState)
--- app/src/main/java/com/example/austin/globalwarmingsim/MainActivity.java
+++ app/src/main/java/com/example/austin/globalwarmingsim/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.austin.globalwarmingsim;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -21,6 +22,26 @@ public class MainActivity extends AppCompatActivity
             public void onClick(View v)
             {
                 Intent playIntent = new Intent(MainActivity.this, PlayActivity.class);
+                startActivity(playIntent);
+            }
+        });
+        Button howToButton = (Button) findViewById(R.id.button_howTo);
+        howToButton.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                Intent playIntent = new Intent(MainActivity.this, HowToActivity.class);
+                startActivity(playIntent);
+            }
+        });
+        Button infoButton = (Button) findViewById(R.id.button_info);
+        infoButton.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                Intent playIntent = new Intent(MainActivity.this, InfoActivity.class);
                 startActivity(playIntent);
             }
         });
