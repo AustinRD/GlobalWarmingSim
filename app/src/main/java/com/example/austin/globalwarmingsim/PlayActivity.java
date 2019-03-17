75a974f2b543aba0acc83a580c7acc4d98c81604
AustinRD <austinrd95@gmail.com>
2019-03-17 00:01:48 -0400

    Restructured play layout file, fixing the overlay, and allowing the user to zoom on the map.

--- app/src/main/java/com/example/austin/globalwarmingsim/PlayActivity.java
+++ app/src/main/java/com/example/austin/globalwarmingsim/PlayActivity.java
@@ -2,14 +2,46 @@ package com.example.austin.globalwarmingsim;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.MotionEvent;
+import android.view.ScaleGestureDetector;
+import android.widget.ImageView;
 
 public class PlayActivity extends AppCompatActivity
 {
+    //Code necessary for scaling the play map. (Zooming)
+    private ScaleGestureDetector mScalreGestureDetector;
+    private float mScaleFactor = 1.0f;
+    private ImageView mImageView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         getSupportActionBar().hide();
         setContentView(R.layout.activtiy_play);
+        mImageView = (ImageView) findViewById(R.id.imageView_playMap);
+        mScalreGestureDetector = new ScaleGestureDetector(this, new ScaleListener());
     }
+    @Override
+    public boolean onTouchEvent(MotionEvent event)
+    {
+        mScalreGestureDetector.onTouchEvent(event);
+        return true;
+    }
+
+    private class ScaleListener extends ScaleGestureDetector.SimpleOnScaleGestureListener
+    {
+        @Override
+        public boolean onScale(ScaleGestureDetector scaleGestureDetector)
+        {
+            mScaleFactor *= scaleGestureDetector.getScaleFactor();
+            mScaleFactor = Math.max(0.1f,Math.min(mScaleFactor, 10.0f));
+            mImageView.setScaleX(mScaleFactor);
+            mImageView.setScaleY(mScaleFactor);
+            return true;
+        }
+    }
+
 }
+
+
