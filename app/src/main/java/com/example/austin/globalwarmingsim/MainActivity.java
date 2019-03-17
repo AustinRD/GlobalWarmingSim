0af6854036e5688105c2270e99ce5c72ca1271dc
AustinRD <austinrd95@gmail.com>
2019-03-17 01:39:21 -0400

    Restructured play layout file, fixing the overlay, and allowing the user to zoom on the map.

--- app/src/main/java/com/example/austin/globalwarmingsim/MainActivity.java
+++ app/src/main/java/com/example/austin/globalwarmingsim/MainActivity.java
@@ -1,13 +1,10 @@
 package com.example.austin.globalwarmingsim;
 
 import android.content.Intent;
-import android.graphics.Path;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import com.github.siyamed.shapeimageview.*;
-import com.github.siyamed.shapeimageview.path.parser.SvgToPath;
 
 public class MainActivity extends AppCompatActivity
 {
