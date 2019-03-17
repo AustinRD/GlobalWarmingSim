2ca1ce01d88dd501d924b96565b69f46ba63bc69
AustinRD <austinrd95@gmail.com>
2019-03-17 01:25:39 -0400

    Restructured play layout file, fixing the overlay, and allowing the user to zoom on the map.

--- app/src/main/java/com/example/austin/globalwarmingsim/PlayActivity.java
+++ app/src/main/java/com/example/austin/globalwarmingsim/PlayActivity.java
@@ -2,6 +2,7 @@ package com.example.austin.globalwarmingsim;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.MotionEvent;
 import android.view.ScaleGestureDetector;
 import android.widget.ImageView;
@@ -9,8 +10,10 @@ import android.widget.ImageView;
 public class PlayActivity extends AppCompatActivity
 {
     //Code necessary for scaling the play map. (Zooming)
-    private ScaleGestureDetector mScalreGestureDetector;
+    private ScaleGestureDetector mScaleGestureDetector;
+    //Global scale factor.
     private float mScaleFactor = 1.0f;
+    //Global map image.
     private ImageView mImageView;
 
     @Override
@@ -20,12 +23,14 @@ public class PlayActivity extends AppCompatActivity
         getSupportActionBar().hide();
         setContentView(R.layout.activtiy_play);
         mImageView = (ImageView) findViewById(R.id.imageView_playMap);
-        mScalreGestureDetector = new ScaleGestureDetector(this, new ScaleListener());
+        mScaleGestureDetector = new ScaleGestureDetector(this, new ScaleListener());
     }
     @Override
     public boolean onTouchEvent(MotionEvent event)
     {
-        mScalreGestureDetector.onTouchEvent(event);
+        mScaleGestureDetector.onTouchEvent(event);
+
+
         return true;
     }
 
@@ -36,8 +41,23 @@ public class PlayActivity extends AppCompatActivity
         {
             mScaleFactor *= scaleGestureDetector.getScaleFactor();
             mScaleFactor = Math.max(0.1f,Math.min(mScaleFactor, 10.0f));
-            mImageView.setScaleX(mScaleFactor);
-            mImageView.setScaleY(mScaleFactor);
+
+            //Logic for restraining the scaling to 1x to 2x.
+            if(mScaleFactor > 1 && mScaleFactor <= 2)
+            {
+                mImageView.setScaleX(mScaleFactor);
+                mImageView.setScaleY(mScaleFactor);
+            }
+            else if(mScaleFactor < 1)
+            {
+                mScaleFactor = 1.0f;
+            }
+            else if(mScaleFactor > 2)
+            {
+                mScaleFactor = 2.0f;
+            }
+            Log.d("LogTag", "mScaleFactor: " + mScaleFactor);
+
             return true;
         }
     }
