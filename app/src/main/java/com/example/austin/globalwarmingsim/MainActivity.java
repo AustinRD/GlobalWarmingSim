31853e1077acc2108c6620ae186f64f1163a4b3b
AustinRD <austinrd95@gmail.com>
2019-03-16 22:16:20 -0400

    Sample Data in information header for play screen.

--- app/src/main/java/com/example/austin/globalwarmingsim/MainActivity.java
+++ app/src/main/java/com/example/austin/globalwarmingsim/MainActivity.java
@@ -1,12 +1,10 @@
 package com.example.austin.globalwarmingsim;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.content.Intent;
 
 public class MainActivity extends AppCompatActivity
 {
