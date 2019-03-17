2284a5666896ccf8b192f91ca41d9e6e76bfd15f
AustinRD <austinrd95@gmail.com>
2019-03-17 03:41:39 -0400

    Changed default orientation to landscape.

--- app/src/main/java/com/example/austin/globalwarmingsim/HowToActivity.java
+++ app/src/main/java/com/example/austin/globalwarmingsim/HowToActivity.java
@@ -2,6 +2,7 @@ package com.example.austin.globalwarmingsim;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 public class HowToActivity extends AppCompatActivity
 {
@@ -11,5 +12,11 @@ public class HowToActivity extends AppCompatActivity
         super.onCreate(savedInstanceState);
         getSupportActionBar().hide();
         setContentView(R.layout.activity_howto);
+
+        TextView howToText = (TextView) findViewById(R.id.textView_howToPlayInfo);
+        howToText.setText("Save the world by 2030!\n\n You will be making decisions that delay " +
+                "or reverse the effects of climate change before the Earth warms above 1.5°C " +
+                "\"the point of no return\" for our climate. Every decision you make will have " +
+                "consequences. \n\nCan you save our civilization?");
     }
 }
